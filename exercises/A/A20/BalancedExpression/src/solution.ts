7f48af8c60270c6846017f0a83213826d770c585
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-13 14:02:41 -0600

    ok/ko

---
+++ exercises/A/A20/BalancedExpression/src/solution.ts
@@ -0,0 +1,28 @@
+
+/**
+ * @return number
+ * @param series
+ */
+
+export function solution(series:string):number {
+  console.log(`series: ${series}`)
+  let stack: string[] = []
+  for (let i of series) {
+    console.log(`i: ${i}`)
+    if (i == '(' || i == '{' || i == '[') {
+      stack.push(i)
+    } else if (i==')' || i==']' || i=='}') {
+      if (stack.length==0){
+        return 0
+      }
+      let top:string = stack[stack.length-1]
+      console.log(`top: ${top}`)
+      if ( (top=='(' && i==')') || (top=='[' && i==']') || (top=='{' && i=='}') ) {
+        stack.pop()
+      } else {
+        return 0
+      }
+    }
+  }
+  return stack.length==0 ? 1 : 0
+}
