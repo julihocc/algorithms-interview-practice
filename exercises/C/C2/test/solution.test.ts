cb07775eb0303e1274b9d6b1589e1967cb1112d9
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-16 18:41:43 -0600

    ko/ok

---
+++ exercises/C/C2/test/solution.test.ts
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import { ListNode, findLoopStart } from "../src/solution";
+
+describe("solution", () => {
+  it("finds the loop start of 1->2->3->4->5->3", () => {
+    // Example usage:
+    let node1 = new ListNode(1);
+    let node2 = new ListNode(2);
+    let node3 = new ListNode(3);
+    let node4 = new ListNode(4);
+    let node5 = new ListNode(5);
+
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;
+    node5.next = node3;  // Creates a loop
+    expect(findLoopStart(node1)!.value).to.equal(3);
+  })
+})
