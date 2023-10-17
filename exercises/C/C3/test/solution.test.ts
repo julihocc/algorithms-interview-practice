5244e6790d5d950116e9055964a8cc5ebc2ecebd
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-17 11:42:30 -0600

    ko/ok

---
+++ exercises/C/C3/test/solution.test.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import { Node, kthToLast } from "../src/solution";
+
+describe("solution", () => {
+  it(" 1 -> 2 -> 3 -> 4 -> 5", () => {
+    const head = new Node(1);
+    head.next = new Node(2);
+    head.next.next = new Node(3);
+    head.next.next.next = new Node(4);
+    head.next.next.next.next = new Node(5);
+    const result = kthToLast(head, 2);
+    expect(result!.data).to.equal(4);
+  })
+})
---
+++ exercises/C/C4/test/solution.test.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import { Node, mergeSort, retrieveList } from "../src/solution";
+
+describe("solution", () => {
+  it("mergeSort 4->2->8->1->5", () => {
+    const head = new Node(4);
+    head.next = new Node(2);
+    head.next.next = new Node(8);
+    head.next.next.next = new Node(1);
+    head.next.next.next.next = new Node(5);
+    const sortedList = mergeSort(head);
+    expect(retrieveList(sortedList)).to.deep.equal([1, 2, 4, 5, 8]);
+  })
+})
