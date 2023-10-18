b9b68956ba44981c36eebecab23911d11424419e
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-18 11:03:12 -0600

    ko/ok

---
+++ exercises/D/D5/test/solution.test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { findMissingNumber } from "../src/solution";
+
+describe("solution", () => {
+  it("does something", () => {
+    expect(findMissingNumber([1, 2, 3, 4, 6, 7, 8])).to.equal(5);
+  })
+})
