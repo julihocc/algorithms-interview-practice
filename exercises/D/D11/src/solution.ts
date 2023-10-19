747741140c2256a6bde3cdfa53144f0bfdffbb0f
Juliho Castillo <julihocc@users.noreply.github.com>
2023-10-19 13:55:11 -0600

    refactored

--- exercises/D/D11/src/solution.ts
+++ exercises/D/D11/src/solution.ts
@@ -3,68 +3,50 @@
  *
  * We use Minimum Window Substring Algorithm to solve this problem.
  */
-function initializeDict(t: string): { [key: string]: number } {
-  let dictT: { [key: string]: number } = {};
-  for (let char of t) {
-    dictT[char] = (dictT[char] || 0) + 1;
-  }
-  return dictT;
-}
 
-function shouldExpand(r: number, sLength: number, formed: number, required: number): boolean {
-  return r < sLength && formed !== required;
-}
-
-function shouldContract(l: number, r: number, formed: number, required: number): boolean {
-  return l <= r && formed === required;
-}
-
-function updateWindowCounts(windowCounts: { [key: string]: number }, char: string, operation: "increment" | "decrement"): void {
-  if (operation === "increment") {
-    windowCounts[char] = (windowCounts[char] || 0) + 1;
-  } else {
-    windowCounts[char] = (windowCounts[char] || 0) - 1;
-  }
-}
-
-export function minWindow(s: string, t: string): string {
-  if (!s || !t) {
-    return "";
-  }
-
-  const dictT = initializeDict(t);
-  const required: number = Object.keys(dictT).length;
-
-  let [l, r, formed] = [0, 0, 0];
-  let windowCounts: { [key: string]: number } = {};
-  let ans: [number, number, number] = [Infinity, 0, 0];
-
-  while (shouldExpand(r, s.length, formed, required)) {
-    const char = s[r];
-    updateWindowCounts(windowCounts, char, "increment");
-
-    if (dictT[char] && windowCounts[char] === dictT[char]) {
-      formed++;
+export function minWindow(word: string, requiredChars: string): string {
+    if (!word || !requiredChars) {
+        return "";
     }
 
-    while (shouldContract(l, r, formed, required)) {
-      const char = s[l];
+    let [leftPointer, rightPointer, currentlyPresent]: number[] = [0, 0, 0];
+    let windowCounts: { [key: string]: number } = {};
 
-      if (r - l + 1 < ans[0]) {
-        ans = [r - l + 1, l, r];
-      }
-
-      updateWindowCounts(windowCounts, char, "decrement");
-
-      if (dictT[char] && windowCounts[char] < dictT[char]) {
-        formed--;
-      }
-
-      l++;
+    let window: { stretch: number; stop: number; start: number } = {
+        stretch: Infinity, start: 0, stop: 0
     }
 
-    r++;
-  }
+    while (rightPointer < word.length && currentlyPresent !== requiredChars.length) {
+        const char: string = word[rightPointer];
+        windowCounts[char] = (windowCounts[char] || 0) + 1;
 
-  return ans[0] === Infinity ? "" : s.slice(ans[1], ans[2] + 1);
+        if (requiredChars.includes(char) && windowCounts[char] == 1) {
+            currentlyPresent++;
+        }
+
+        while (leftPointer <= rightPointer && currentlyPresent === requiredChars.length) {
+
+            let lengthCurrentWindow: number = rightPointer - leftPointer + 1;
+
+            if (lengthCurrentWindow < window.stretch) {
+                window.stretch = lengthCurrentWindow;
+                window.start = leftPointer;
+                window.stop = rightPointer;
+            }
+
+            const char: string = word[leftPointer];
+
+            windowCounts[char] = (windowCounts[char] || 0) - 1;
+
+            if (requiredChars.includes(char) && windowCounts[char] == 0) {
+                currentlyPresent--;
+            }
+
+            leftPointer++;
+        }
+
+        rightPointer++;
+    }
+
+    return window.stretch === Infinity ? "" : word.slice(window.start, window.stop + 1);
 }
